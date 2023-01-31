78d2afba6684e25dd557db1d04ae6d829598da02
Shani <shanigeva12@gmail.com>
2023-01-31 21:15:59 +0200

    building foundation to color picker

--- src/app/shared/components/color-picker/color-picker.component.ts
+++ src/app/shared/components/color-picker/color-picker.component.ts
@@ -1,11 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
   styleUrls: ['./color-picker.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ColorPickerComponent,
+      multi: true
+    }]
 })
-export class ColorPickerComponent {
+export class ColorPickerComponent implements ControlValueAccessor {
+  isOpen = false;
+  disabled = false;
+  onChanged: any = () => {};
+  onTouched: any = () => {};
+  colorValue: any;
+
+  writeValue(obj: any): void {
+    this.colorValue = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChanged = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 
 }
