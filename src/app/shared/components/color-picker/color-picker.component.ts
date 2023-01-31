82a41d7cbf392437d1b7a9938e1bf87a618dbee2
Shani <shanigeva12@gmail.com>
2023-01-31 20:47:14 +0200

    created color-picker comp

---
+++ src/app/shared/components/color-picker/color-picker.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-picker',
+  templateUrl: './color-picker.component.html',
+  styleUrls: ['./color-picker.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ColorPickerComponent {
+
+}
