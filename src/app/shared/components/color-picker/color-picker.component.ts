b3d8524c8fa6f0491efd79b1119609694255f966
Shani <shanigeva12@gmail.com>
2023-02-01 16:02:46 +0200

    working color picker

--- src/app/shared/components/color-picker/color-picker.component.ts
+++ src/app/shared/components/color-picker/color-picker.component.ts
@@ -31,17 +31,6 @@ export class ColorPickerComponent implements OnInit, ControlValueAccessor {
     new ColorRgba(0, 0, 0, 1),
   ];
   basicColors: string[] = this.basicRGBA.map(item=> item.getColorString() || '');
-  // [
-  //   'rgba(250, 235, 59, 1)',
-  //   'rgba(38, 150, 136, 1)',
-  //   'rgba(58, 188, 212, 1)',
-  //   'rgba(64, 169, 244, 1)',
-  //   'rgba(62, 150, 243, 1)',
-  //   'rgba(103, 58, 183, 1)',
-  //   'rgba(233, 30, 98, 1)',
-  //   'rgba(244, 67, 54, 1)',
-  //   'rgba(0, 0, 0, 1)',
-  // ];
 
   colorsPaletteRgba: ColorRgba[][] = [];
   colorsPaletteString: string[][] = this.colorsPaletteRgba.map(item=> item.map(item=> item.getColorString() || ''));
@@ -55,18 +44,15 @@ export class ColorPickerComponent implements OnInit, ControlValueAccessor {
   colorValue: any;
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    console.log(this.basicColors);
     this.colorsPaletteRgba = this.createColorsPalette(this.basicRGBA);
-    console.log(this.colorsPaletteRgba);
+    this.colorsPaletteString = this.colorsPaletteRgba.map(item=> item.map(item=> item.getColorString() || ''));
   }
 
   createColorsPalette(basicRgbaColors: ColorRgba[]) : ColorRgba[][] {
     let colorsPalette: ColorRgba[][] = [];
     for(let i = 0; i < basicRgbaColors.length; i++) {
       colorsPalette[i] = [];
-      for(let j = 1; j< 10; j++) {
+      for(let j = 0; j< 9; j++) {
         colorsPalette[i][j] = ColorRgba.createFromColor(basicRgbaColors[i], j*0.1);
       }
     }
