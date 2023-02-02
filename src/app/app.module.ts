fdf35552f8d8dfb802f2b78617561d434095b929
Shani <shanigeva12@gmail.com>
2023-02-02 21:24:01 +0200

    check tags comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TagsComponent } from './components/tags/tags.component';
+import { AddTagComponent } from './components/tags/components/add-tag/add-tag.component';
+import { MaterialModule } from './shared/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TagsComponent,
+    AddTagComponent,
 
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
     SharedModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  exports: [
+  ],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
