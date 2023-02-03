efbd26808c3bb445ab4a1a187252d96a46469468
Shani <shanigeva12@gmail.com>
2023-02-03 17:59:28 +0200

    hadling row clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { SubSink } from 'subsink';
 import { ColorRgba } from './shared/model/color-rgba';
+import { Tag } from './components/tags/model/tag.model';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,13 @@ import { ColorRgba } from './shared/model/color-rgba';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild( MatDrawer)
+  drawer!:  MatDrawer;
+
   title = 'data-manage-app';
   subs = new SubSink();
+
+  tagToEdit: Tag | undefined;
   // color = new FormControl();
   // myColor = new ColorRgba(103, 58, 183, 1);
   // myStringColor = 'rgba(244, 67, 54, 1)';
@@ -26,5 +33,15 @@ export class AppComponent {
 
   ngOnDestroy() {
     this.subs.unsubscribe();
+  }
+
+  createNewTag() {
+    this.tagToEdit = undefined;
+    this.drawer.toggle();
+  }
+
+  openEditTag(tag: Tag) {
+    this.tagToEdit = tag;
+    this.drawer.open();
   }
 }
