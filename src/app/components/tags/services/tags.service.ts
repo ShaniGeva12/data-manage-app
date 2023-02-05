c4e34ea4a27c2d127033a400816daa81080eb416
Shani <shanigeva12@gmail.com>
2023-02-05 23:51:43 +0200

    added subsink

--- src/app/components/tags/services/tags.service.ts
+++ src/app/components/tags/services/tags.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
 import { AddTagRequest, Tag } from '../model/tag.model';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { SubSink } from 'subsink';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TagsService {
   private readonly TagsServiceUrl = 'http://localhost:3002/tags';
@@ -12,59 +13,62 @@ export class TagsService {
   private _tags$: BehaviorSubject<Tag[]> = new BehaviorSubject<Tag[]>([]);
   public tags$: Observable<Tag[]> = this._tags$.asObservable();
 
+  subs: SubSink = new SubSink();
+
   constructor(private http: HttpClient) {
     this.refreshTagsList();
   }
 
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
   private refreshTagsList(): void {
-    this.getTags().subscribe(res => {
+    this.subs.sink = this.getTags().subscribe((res) => {
       this._tags$.next(res);
     });
   }
 
   private getTags(): Observable<Array<Tag>> {
-    return this.http.get<Array<Tag>>(this.TagsServiceUrl)
-    .pipe(
-      catchError(err => this.handleError(err, 'getTags'))
-    );
+    return this.http
+      .get<Array<Tag>>(this.TagsServiceUrl)
+      .pipe(catchError((err) => this.handleError(err, 'getTags')));
   }
 
   private postTag(tag: AddTagRequest): Observable<Tag> {
-    return this.http.post<Tag>(this.TagsServiceUrl, tag)
-    .pipe(
-      catchError(err => this.handleError(err, 'postTag', tag))
-    );
+    return this.http
+      .post<Tag>(this.TagsServiceUrl, tag)
+      .pipe(catchError((err) => this.handleError(err, 'postTag', tag)));
   }
 
   private putTag(tag: Tag): Observable<Tag> {
-    return this.http.put<Tag>( `${this.TagsServiceUrl}/${tag.id}` , <AddTagRequest>tag)
-      .pipe(
-        catchError(err => this.handleError(err, 'putTag', tag))
-      );
+    return this.http
+      .put<Tag>(`${this.TagsServiceUrl}/${tag.id}`, <AddTagRequest>tag)
+      .pipe(catchError((err) => this.handleError(err, 'putTag', tag)));
   }
 
-  private handleError(error: HttpErrorResponse, methodName? : string, obj? : any) : Observable<never> {
+  private handleError(
+    error: HttpErrorResponse,
+    methodName?: string,
+    obj?: any
+  ): Observable<never> {
     if (error.status === 0) {
       // A client-side or network error occurred
       console.error('An error occurred:', error.error);
     } else {
       console.error(
-        `Backend returned code ${error.status} from method "${methodName}". Full error and obj: `, { Error: error, ObjectSent: obj });
+        `Backend returned code ${error.status} from method "${methodName}". Full error and obj: `,
+        { Error: error, ObjectSent: obj }
+      );
     }
     return throwError(() => error);
   }
 
-  addTag(tag: AddTagRequest) : Observable<Tag> {
-    return this.postTag(tag)
-      .pipe(
-        tap(() => this.refreshTagsList()),
-      );
+  addTag(tag: AddTagRequest): Observable<Tag> {
+    return this.postTag(tag).pipe(tap(() => this.refreshTagsList()));
   }
 
-  updateTag(tag: Tag) : Observable<Tag> {
-    return this.putTag(tag)
-      .pipe(
-        tap(() => this.refreshTagsList()),
-      )
+  updateTag(tag: Tag): Observable<Tag> {
+    return this.putTag(tag).pipe(tap(() => this.refreshTagsList()));
   }
 }
