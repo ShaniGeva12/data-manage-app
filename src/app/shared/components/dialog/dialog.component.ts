bee01aa3bd2f02d5d83d1fa93b43fa8289991d45
Shani <shanigeva12@gmail.com>
2023-02-05 14:40:17 +0200

    added dialog msg for errors

---
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, Inject, TemplateRef } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from '../../model/dialog-data';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<any>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
